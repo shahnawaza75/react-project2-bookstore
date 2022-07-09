8d61320dc451b414307b1c30d699b12fcb95bc9a
Shahnawaz Ali <shahnawaza75@gmail.com>
2022-07-09 23:06:15 +0530

    created components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import BookList from './components/BookList';
+import Categories from './components/Categories';
+import Navigation from './components/Navigation';
 
 function App() {
   return (
     <div className="App">
-    hello
+      <Router>
+        <Navigation />
+        <Routes>
+          <Route path="/" element={<BookList />} />
+          <Route path="/categories" element={<Categories />} />
+        </Routes>
+      </Router>
+
     </div>
   );
 }
