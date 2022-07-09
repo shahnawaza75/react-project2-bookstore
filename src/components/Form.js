5d914b7cc641dea428c25fa63074e3dece74b4d8
Shahnawaz Ali <shahnawaza75@gmail.com>
2022-07-09 23:07:44 +0530

    added extra input

--- src/components/Form.js
+++ src/components/Form.js
@@ -6,11 +6,6 @@ const Form = () => (
     <form>
       <input type="title" />
       <input type="author" />
-      <select name="categorie" id="categorie">
-        <option value="category1">category1</option>
-        <option value="category2">category1</option>
-        <option value="category3">category3</option>
-      </select>
       <button type="submit">ADD BOOK</button>
     </form>
   </>
